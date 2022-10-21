562f3d8d2796620593081df6b96c568755bf3077
DmitryVilenchik <vilend1969@gmail.com>
2022-10-21 15:22:58 +0300

    HW9

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const detailsImage=document.querySelector(".details-image");
 const detailsTitle=document.querySelector(".details-title");
 
 const detailsFrame=document.querySelector(".details-frame"); /*  my cod  */
+const detailsAnchor=document.querySelector("details-anchor"); 
 
 const mainClass = document.querySelector(".main-class");
 const HIDDEN = "hidden";
@@ -11,6 +12,7 @@ const HIDDEN = "hidden";
 function setDetails(anchor) {
     detailsImage.setAttribute("src", anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML=anchor.getAttribute('data-details-title');
+    /*detailsAnchor.setAttribute('details-url', anchor.getAttribute('data-details-url'));*/
 }
 for(let i=0; i<anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
@@ -20,25 +22,30 @@ for(let i=0; i<anchors.length; i++) {
     })
 }
 detailsFrame.addEventListener("click", function() { /*   my cod     */  
-
+    
+    /*window.open(detailsAnchor.getAttribute('details-url'));*/
     let a=detailsTitle.innerHTML;
-   
+   let url1="https://en.wikipedia.org/wiki/Stater";
+   let url2="https://en.wikipedia.org/wiki/Sovereign_(British_coin)";
+   let url3="https://en.wikipedia.org/wiki/Coin";
+   let url4="https://en.wikipedia.org/wiki/Banknote";
+   let url5="https://en.wikipedia.org/wiki/Money";
     console.log(a);
     switch(a) {
         case "Electrum coin":
-            window.open("https://en.wikipedia.org/wiki/Stater","_blank");
+            window.open(url1,"_blank");
             break;
         case "Gold sovereign":
-            window.open("https://en.wikipedia.org/wiki/Sovereign_(British_coin)","_blank");
+            window.open(url2,"_blank");
             break;
         case "Gold coins":
-            window.open("https://en.wikipedia.org/wiki/Coin","_blank");
+            window.open(url3,"_blank");
             break;
         case "Banknotes":
-            window.open("https://en.wikipedia.org/wiki/Banknote","_blank");
+            window.open(url4,"_blank");
             break;
         default:
-            window.open("https://en.wikipedia.org/wiki/Money","_blank");         
+            window.open(url5,"_blank");         
     }
    
 })
