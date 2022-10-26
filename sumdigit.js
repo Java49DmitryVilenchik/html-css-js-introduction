e5e947f11a255e606a2e8ad37393a82792ac860f
DmitryVilenchik <vilend1969@gmail.com>
2022-10-26 23:39:25 +0300

    final HomeWork 11

--- sumdigit.js
+++ sumdigit.js
@@ -1,5 +1,12 @@
 function sumDigits(number) {
     if (number<0) {number=-number};
+    if (number%1!=0) {
+        
+        do {          
+          number=number*10;
+        } while (number%1!=0)
+        
+    }
     let sum=0;
     do {
         let i=number % 10;
@@ -8,6 +15,12 @@ function sumDigits(number) {
     }while(number!=0);
     return sum;
 }
+
+
 console.log(sumDigits(624));
 console.log(sumDigits(-123));
 console.log(sumDigits(0));
+console.log(sumDigits(-12.3467));
+console.log(sumDigits(12.3467));
+console.log(sumDigits(-0.23573));
+
