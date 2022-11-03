03f91b8daec23df834763b28afb7446191fa6b1d
DmitryVilenchik <vilend1969@gmail.com>
2022-11-03 23:28:12 +0200

    test hw13

--- hw13.js
+++ hw13.js
@@ -22,8 +22,32 @@ function checkTeudatZehut(teudatStrNumber) {
     let check=(sum % 10==0) ? true : false;
     return check;
 }
+
+function generateRandomTeudZehut() {
+    let Arr=[Math.round(Math.random() * 9),Math.round(Math.random() * 9),
+        Math.round(Math.random() * 9),Math.round(Math.random() * 9),Math.round(Math.random() * 9),
+        Math.round(Math.random() * 9),Math.round(Math.random() * 9),Math.round(Math.random() * 9)];
+       
+        let Arr1=Arr.map(function(symbol, index) {
+            
+        if (index%2!=0) {symbol=symbol*2;
+        if (symbol/10>=1) {let s1=symbol%10; let s2=Math.trunc(symbol/10); symbol=s1+s2}}
+        return symbol;
+        });
+
+        let sum=Arr1.reduce(function(res, cur) {
+            return res+cur;
+        },0);
+
+        if (sum%10==0) {Arr[8]=0;} else {Arr[8]=10-(sum % 10);}
+        let str=Arr.join('');
+    return str;
+}
 console.log(checkTeudatZehut("123456782")); //true
 console.log(checkTeudatZehut("123456781")); //false
 console.log(checkTeudatZehut("-23456781")); //false
 console.log(checkTeudatZehut("6781")); //false
 console.log(checkTeudatZehut("6781335423242442432")); //false
+
+console.log(generateRandomTeudZehut());
+console.log(checkTeudatZehut(generateRandomTeudZehut()));
