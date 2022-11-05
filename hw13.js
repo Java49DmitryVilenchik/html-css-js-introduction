b0bd00012d2b034b7c42a1bacdb6b856a255249d
DmitryVilenchik <vilend1969@gmail.com>
2022-11-05 19:04:45 +0200

    Homework 13 -teydat-zeyt- Final

--- hw13.js
+++ hw13.js
@@ -2,8 +2,7 @@ function CheckSymbolsArray (str) {
     let arr=Array.from(str);
     let res=true;
     function checkSymbols(element, array) {
-        let symb=(element>='0' && element<='9') ? true : false;
-        if (symb==false) {res=false;}
+        let symb=(element>='0' && element<='9') ? res=true : res=false;        
     }
     arr.forEach(checkSymbols);
     if (arr.length!=9) {res=false;}
@@ -12,24 +11,14 @@ function CheckSymbolsArray (str) {
 }
 
 function ArrayConv (Arr) {
-    let ArrConv=Arr.map(function(symbol, index) {
-        let s=symbol.charCodeAt(0)-48;
-    if (index%2!=0) {s=s*2;
-    if (s/10>=1) {let s1=s%10; let s2=Math.trunc(s/10); s=s1+s2}}
+    let ArrConv=Arr.map(function(symbol, index) {        
+        let s=+symbol;
+    if (index%2!=0) {s=s*2;    
+    if (s>9) {s-=9}}
     return s;
     });
 
     return ArrConv;
-}
-
-function ArrayConv1 (Arr) {
-    let Arr1=Arr.map(function(symbol, index) {
-            
-        if (index%2!=0) {symbol=symbol*2;
-        if (symbol/10>=1) {let s1=symbol%10; let s2=Math.trunc(symbol/10); symbol=s1+s2}}
-        return symbol;
-        });
-        return Arr1;
 }
 
 function CheckSum (Arr) {
@@ -46,26 +35,30 @@ function checkTeudatZehut(teudatStrNumber) {
     
     let Arr=ArrayConv(Array.from(teudatStrNumber));
     let sum=CheckSum(Arr);
-    if (sum%10!=0) {check=false;}
-    return check;
+    
+    return check=(sum%10==0);
+}
+function generateRandomArray_1_9(leng) {
+    let Ar=Array.from(Array(leng).keys());
+    let Arr=Ar.map(function randomArray(symbol, index) {
+        symbol=(Math.round(Math.random() * 9))+'';
+        return symbol;
+    });
+    return Arr;
 }
 
 function generateRandomTeudZehut() {
-    let Arr=[Math.round(Math.random() * 9),Math.round(Math.random() * 9),
-        Math.round(Math.random() * 9),Math.round(Math.random() * 9),Math.round(Math.random() * 9),
-        Math.round(Math.random() * 9),Math.round(Math.random() * 9),Math.round(Math.random() * 9)];
+    let Arr=generateRandomArray_1_9(8);
+    let Arr1=ArrayConv(Arr);
 
-    let Arr1=ArrayConv1(Arr);
-    
-    let s=CheckSum(Arr1);
-    if (s%10==0) {Arr[8]=0;} else {Arr[8]=10-(s % 10);}
-    
-    let str=Arr.join('');
-    
-    return str;
+    let s=CheckSum(Arr1);    
+    Arr[8]=(s%10==0) ? 0 : 10-(s % 10);    
+    return Arr.join('');    
 }
 
+console.log(checkTeudatZehut("232572453")); //true
 console.log(checkTeudatZehut("123456782")); //true
+console.log(checkTeudatZehut("qwNbHryh!")); //false
 console.log(checkTeudatZehut("123456781")); //false
 console.log(checkTeudatZehut("-23456781")); //false
 console.log(checkTeudatZehut("6781")); //false
@@ -74,3 +67,4 @@ console.log(checkTeudatZehut("6781335423242442432")); //false
 console.log(generateRandomTeudZehut());
 
 console.log(checkTeudatZehut(generateRandomTeudZehut()));
+console.log(checkTeudatZehut("814811410"));
