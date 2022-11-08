9aee8acefe5d773eeda769b7ca1d3a317f51f3bd
DmitryVilenchik <vilend1969@gmail.com>
2022-11-09 00:04:21 +0200

    Final

--- hw14.js
+++ hw14.js
@@ -16,14 +16,20 @@ function count(array, str) {
     });
     return count;
 }
-
+//variant 2
+function count2(array, str) {   
+    let count = array.reduce( (arr, e) => {
+      arr[e] = (arr[e] || 0) + 1;      
+      return arr;
+    }, {});
+    return count[str];
+    }
 function arrayCopy(src,srcPos,dst,dstPos,leng) {    
     let a=src.slice(srcPos,(srcPos+leng));    
     a.forEach(function(element){
         dst.splice(dstPos,0,element);
         dstPos++;
-    });   
-     
+    });        
     return dst;
 }
 function move(array,index,offset) {
@@ -36,6 +42,7 @@ function move(array,index,offset) {
 
 console.log(ulSurround(["I","like","JS"]));
 console.log(count(["cd","ab","cd","ab","ty","ab"],"ab"));
+console.log(count2(["cd","ab","cd","ab","ty","ab"],"ab"));
 console.log(arrayCopy([1,2,3,4,5,6,7],3,[10,20,30,40,50,60,70],4,3));
 console.log(move([1,2,3,4,5,6,7],3,-1));
 console.log(move([1,2,3,4,5,6,7],2,4));
