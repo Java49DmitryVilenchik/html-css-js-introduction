74a628141ecbdad439522cdc0ddf57d1ca047fa2
DmitryVilenchik <vilend1969@gmail.com>
2022-11-21 23:23:57 +0200

    hw18 itogo

--- hw18.js
+++ hw18.js
@@ -1,42 +1,31 @@
-function an(string1,string2) {
-    let ar1=Array.from(string1);
-    let ar2=Array.from(string2);
+
+function isAn(str1,str2) {
+    let ar1=Array.from(str1);
+    let ar2=Array.from(str2);
+
+    let res=true;
 
     let A1={};
-    let A2={};
-
-    if (ar1.length==ar2.length) {
-        return true;
-    }
-    else {return false;}
-
-}
-
-function preobr(ar1,ar2) {
-    let A1={};
-    let A2={};
     
-    ar1.forEach(element => {
-        if (A1[element]) {A1[element]++}
-        else {A1[element]=1}        
-    });
 
-    ar1.forEach(element => {
-        if (A2[element]) {A2[element]++}
-        else {A2[element]=1}        
-    });
+    if (ar1.length!=ar2.length) {res=false;}
+    else {
+        ar1.forEach(element => {
+            if (A1[element]) {A1[element]++}
+            else {A1[element]=1}          
 
-    A1a=Object.entries(A1);
-    A2a=Object.entries(A2);
-    
-    //console.log(A1a[1][1]-A2a[1][1]);
-    let res=A1a.reduce(function(e){
-        //e=e[1]-A2a[1];
+        });
+        ar2.forEach(element => {
+            if (A1[element]) {A1[element]--;}
+            else {res=false;}                
+        });
         
-        return e[1]-A2a.e[1];
-    })
+    } 
+    
     return res;
 }
+console.log(isAn("yelllw","welloy"));
+console.log(isAn("yellow","wellly"));
+console.log(isAn("yellw","welloy"));
 
-console.log(preobr(Array.from("yellow"),Array.from("welloy")));
-
+console.log(isAn("yellow","welloy"));
