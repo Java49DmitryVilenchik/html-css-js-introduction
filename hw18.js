4dc0105ff8c27a3d7c98d71ea2bb72e283f64543
DmitryVilenchik <vilend1969@gmail.com>
2022-11-22 19:50:14 +0200

    ok

--- hw18.js
+++ hw18.js
@@ -3,16 +3,11 @@ function isAn(str1,str2) {
     let res=true;
     if (str1.length!=str2.length) {res=false;}
     else {
-
-    let ar1=Array.from(str1.toLowerCase());
-    let ar2=Array.from(str2.toLowerCase());
-
-    
-
-    let A1={};
-    
-
-    
+        let ar1=Array.from(str1.toLowerCase());
+        let ar2=Array.from(str2.toLowerCase());
+        
+        let A1={};
+            
         ar1.forEach(element => {
             if (A1[element]) {A1[element]++}
             else {A1[element]=1}          
