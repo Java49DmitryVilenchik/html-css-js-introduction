4629eb1b539a9f038d48d03571a70d87c48a3efa
DmitryVilenchik <vilend1969@gmail.com>
2022-11-23 16:02:18 +0200

    add new variant -better

--- hw18.js
+++ hw18.js
@@ -13,6 +13,7 @@ function isAn(str1,str2) {
             else {A1[element]=1}          
 
         });
+
         ar2.forEach(element => {
             if (A1[element]) {A1[element]--; if (A1[element]<0) {res=false;}}
             else {res=false;}
@@ -23,9 +24,39 @@ function isAn(str1,str2) {
     
     return res;
 }
+
+function isAn2(str1,str2) {
+    let res=true;
+    if (str1.length!=str2.length) {res=false;}
+    else {
+        let ar1=Array.from(str1.toLowerCase());
+        let ar2=Array.from(str2.toLowerCase());
+        
+        let A1={};
+            
+        ar1.forEach(element => {
+            if (A1[element]) {A1[element]++}
+            else {A1[element]=1}          
+
+        });
+
+        let index=0;
+        while (index<ar2.length && res) {
+            if (A1[ar2[index]]==0) {res=false;}
+            else {A1[ar2[index]]--; index++;}
+        }        
+    }     
+    return res;
+}
+
+
 console.log(isAn("yelllw","welloy"));
 console.log(isAn("yellow","wellly"));
 console.log(isAn("yellw","welloy"));
+console.log(isAn("sun","sub"));
 
 console.log(isAn("yellow","welloy"));
 console.log(isAn("YelLow","wEllOy"));
+
+console.log(isAn2("yellow","welloy"));
+console.log(isAn2("yellw","welloy"));
