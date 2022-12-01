d43902190e2633b510fa2a6fc60025c091a7ea05
DmitryVilenchik <vilend1969@gmail.com>
2022-12-01 17:59:45 +0200

    1 -ok

--- src/form.js
+++ src/form.js
@@ -13,6 +13,8 @@ function onChange(event) {
     if (event.target.name == "salary") {
         if (+event.target.value<1000 || +event.target.value>40000) {
             event.target.value=''
+           do {event.target.value= prompt("You must input coorect salary: more 999 and less 40000!!!!", '1000');}
+           while (event.target.value<1000 || event.target.value>40000);
         }
     }
 }
