494eb69b623c17fafb03a6b2f36a86f7c4cf5e68
DmitryVilenchik <vilend1969@gmail.com>
2022-12-01 19:54:23 +0200

    need create own alert on 5s

--- src/form.js
+++ src/form.js
@@ -1,5 +1,6 @@
 const inputElements=document.querySelectorAll(".form-class [name]");
 
+
 function onSubmit(event) {
     event.preventDefault();
     console.log("submitted");
@@ -15,6 +16,17 @@ function onChange(event) {
             event.target.value=''
            do {event.target.value= prompt("You must input coorect salary: more 999 and less 40000!!!!", '1000');}
            while (event.target.value<1000 || event.target.value>40000);
+        }
+    }
+}
+function onSelectDate(event) {
+    if (event.target.name == "birthDate") {
+        let year=(event.target.value).split("-");
+        //alert(event.target.value);
+        //alert(year[0]);
+        if (year[0]<1950 || year[0]>2022) {
+            alert("Not ok!");
+            event.target.value='';
         }
     }
 }
