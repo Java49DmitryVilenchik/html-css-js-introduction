f55b6eae3e0e2cbd39e56af211db7d2ad1da86b8
DmitryVilenchik <vilend1969@gmail.com>
2022-12-04 20:40:09 +0200

    rectangle object

---
+++ app.js
@@ -0,0 +1,14 @@
+function Rectangle(width,height) {
+    this.width=width;
+    this.height=height;
+}
+Rectangle.prototype.square=function() {
+    return this.width*this.height;
+}
+Rectangle.prototype.perimeter=function() {
+    return this.width*2+this.height*2;
+}
+const rectangle=new Rectangle(3,5);
+console(rectangle.square());
+console(rectangle.perimeter());
+//re.displ();
